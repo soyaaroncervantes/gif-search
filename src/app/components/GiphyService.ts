682f144d5d11066005d56ee056c243fb726fa6b2
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2025-02-12 14:15:51 -0600

    fix: update ignore file and api key

--- src/app/components/GiphyService.ts
+++ src/app/components/GiphyService.ts
@@ -4,9 +4,10 @@ import {GifSearchModel} from "../mapper/giphy.model.ts";
 
 const urlString = `https://api.giphy.com/v1/gifs/search`
 const url = new URL(urlString);
+const apiKey = import.meta.env.GIPHY_API_KEY;
 
 export const fetchGifs = async (category: string) => {
-  url.searchParams.set('api_key', 'OiLMxXHfv1XKzW78LZNnRRVsrttwLerr');
+  url.searchParams.set('api_key', apiKey);
   url.searchParams.set('q', category);
   url.searchParams.set('limit', '10');
   const fetchedGifs = await fetch(url);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly GIPHY_API_KEY: string;
+
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
